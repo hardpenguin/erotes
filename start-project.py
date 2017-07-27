ad3cdb4c8345768f4425fcfc665703d5ee757f50
hardpenguin <hardpenguin13@gmail.com>
2017-07-27 16:45:44 +0200

    downloading is more elegant now

--- start-project.py
+++ start-project.py
@@ -4,13 +4,39 @@ import sys
 import os
 
 import urllib
-            
+
 ScriptDir=os.path.dirname(os.path.abspath(sys.argv[0]))
 
-print "Downloading love..."
-LoveFolder="love"
-if not os.path.exists(ScriptDir+"/"+LoveFolder):
-    os.makedirs(ScriptDir+"/"+LoveFolder)
-Link="https://bitbucket.org/rude/love/downloads/love_0.10.2ppa1_amd64.deb"
-FileName=Link.split("downloads/")[1]
-urllib.urlretrieve(Link,ScriptDir+"/"+LoveFolder+"/"+FileName)
+class DownloadedFile(object): # downloads the file from a chosen URL and checks its name
+    
+    def __init__(self,Link):
+        self.Link=Link
+        self.Name=self.Link.split("downloads/")[1]
+
+    def Download(self,Destination):
+        self.Destination=Destination
+        urllib.urlretrieve(self.Link,self.Destination)
+        # I gave up on handling download errors as bitbucket willingly serves a HTML file for wrong links
+
+class CreatedFolder(object): # creates folder and returns its absolute path
+
+    def __init__(self,Name):
+        self.Name=Name
+
+    def Create(self,Path):
+        self.Path=Path
+        if not os.path.exists(self.Path+"/"+self.Name):
+            os.makedirs(self.Path+"/"+self.Name)
+        return(self.Path+"/"+self.Name)
+
+LovePath=CreatedFolder("love").Create(ScriptDir)
+
+LinksForDownload=[
+                    "https://bitbucket.org/rude/love/downloads/love_0.10.2ppa1_amd64.deb",
+                    "https://bitbucket.org/rude/love/downloads/liblove0_0.10.2ppa1_amd64.deb"
+                 ]
+
+for Link in LinksForDownload:
+    D=DownloadedFile(Link)
+    D.Download(LovePath+"/"+D.Name)
+    print "Downloading "+D.Name+"..."
