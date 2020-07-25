709a83ce3a38ff90fa009f1ccdef8fc8d1ebd8d6
hardpenguin <hardpenguin13@gmail.com>
2020-07-25 16:12:40 +0200

    some minor improvements to versioning.py

--- utils/versioning.py
+++ utils/versioning.py
@@ -1,23 +1,18 @@
-import datetime
+import time
 
 class Version(object):
     
     def __init__(self, file_path):
         self.file_path = file_path
         self.number = None
-        self._read_from_file()
 
-    def _read_from_file(self):
+    def read_from_file(self):
         f = open(self.file_path, "r")
         self.number = f.read()
         f.close()
 
     def generate_version(self, number):
-        date_now = datetime.datetime.utcnow()
-        date_epoch = datetime.datetime(1970,1,1)
-        date_diff = date_now - date_epoch
-        epoch_days = str(date_diff.days)
-        
+        epoch_days = str(time.time()/60/60/24).split(".")[0]
         f = open(self.file_path, "w")
         self.number = "%s.%s" %(str(number), epoch_days)
         f.write(self.number)
